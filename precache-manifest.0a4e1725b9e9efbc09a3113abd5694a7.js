b7d57d5eb7de5b417522abc27fd7caf615a98b7b
orgad <orgad@svcollege.co.il>
2020-07-20 20:20:09 +0300

    Updates

---
+++ precache-manifest.0a4e1725b9e9efbc09a3113abd5694a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e108355776a182ed29fdde9962cf3e65",
+    "url": "/reactRep2/index.html"
+  },
+  {
+    "revision": "a0cdc341de32f15d68ef",
+    "url": "/reactRep2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f8f09b9edfd47f70ac67",
+    "url": "/reactRep2/static/js/2.8ecaccd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactRep2/static/js/2.8ecaccd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0cdc341de32f15d68ef",
+    "url": "/reactRep2/static/js/main.89beea7e.chunk.js"
+  },
+  {
+    "revision": "4f09cd7658a53873e68a",
+    "url": "/reactRep2/static/js/runtime-main.df2cd1f0.js"
+  }
+]);
